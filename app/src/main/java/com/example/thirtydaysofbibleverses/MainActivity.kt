32d503ca767a40f132b4f5baca65fd7c3f13d286
jcisking <krisloeffler@gmail.com>
2023-01-16 01:50:54 -0700

    another commit

--- app/src/main/java/com/example/thirtydaysofbibleverses/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysofbibleverses/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.thirtydaysofbibleverses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thirtydaysofbibleverses.data.TipRepository
 import com.example.thirtydaysofbibleverses.ui.theme.ThirtyDaysOfBibleVersesTheme
 
@@ -26,6 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    BibleVerseApp()
 
                 }
             }
@@ -33,9 +35,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun BibleVerseApp() {
-    LazyRow() {
+    LazyColumn() {
         items(TipRepository.tips) {
             TipItem(tip = it)
         }
